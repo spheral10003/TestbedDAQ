8e3fc57a8b5ebac29eba86f0c52920ba6bf12ba0
aodgkf <aodgkf@naver.com>
2022-08-25 19:47:25 +0900

    김명규 테스트

--- TestbedDAQ/Forms/frmMCManager.cs
+++ TestbedDAQ/Forms/frmMCManager.cs
@@ -8,14 +8,18 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using Library;
+using TestbedDAQ.UseClass;
 
 namespace TestbedDAQ.Forms
 {
     public partial class frmMCManager : Form
     {
         private List<MachineInformation> _MachineInformation;
+        private List<PLCAddressMap> _PLCAddressMap;
+
 
         public List<MachineInformation> MachineInformationList { get => _MachineInformation; set => _MachineInformation = value; }
+        public List<PLCAddressMap> PLCAddressMap { get => _PLCAddressMap; set => _PLCAddressMap = value; }
 
 
         public frmMCManager()
@@ -25,11 +29,29 @@ namespace TestbedDAQ.Forms
 
         private void frmMCManager_Load(object sender, EventArgs e)
         {
-            frmMain frm = (frmMain)this.Owner;
-
+            #region 주석
+            //frmMain frm = (frmMain)this.Owner;
             //frm.MachineInformationList = _MachineInformation;
-
             //frm.label3_Click(null, null);
+            #endregion
+
+            try
+            {
+                TestbedDB _db = new TestbedDB();
+
+                //CtrInit();
+                //CtrComboSetting();
+                //Search();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, ex.Source);
+                return;
+            }
+            finally
+            {
+
+            }
         }
     }
 }
