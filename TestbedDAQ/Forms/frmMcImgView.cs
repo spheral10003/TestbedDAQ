5b2fcc3d2f75e853903f6cd7f48b12f72184c8fc
aodgkf <aodgkf@naver.com>
2022-08-31 09:45:12 +0900

    20220831_3_McImgView_수정_김명규

--- TestbedDAQ/Forms/frmMcImgView.cs
+++ TestbedDAQ/Forms/frmMcImgView.cs
@@ -29,75 +29,146 @@ namespace TestbedDAQ.Forms
 
         public frmMcImgView(string sPath, string sName)
         {
-            InitializeComponent();
+            try
+            {
+                InitializeComponent();
 
-            this._Path = sPath;
-            this._Name = sName;
-            this._FullPath = sPath + "\\" + _Name;
+                this._Path = sPath;
+                this._Name = sName;
+                this._FullPath = sPath + "\\" + _Name;
 
-            pictureBox1.MouseWheel += MouseWheelEvent;
-            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+                pictureBox1.MouseWheel += MouseWheelEvent;
+                pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, ex.Source);
+                return;
+            }
+            finally
+            {
+            
+            }
         }
 
         private void frmMcImgView_Load(object sender, EventArgs e)
         {
-            if (_FullPath != string.Empty)
+            try
             {
-                WebClient ftpClient = new WebClient();
-                ftpClient.Credentials = new NetworkCredential(_Ftp._UserId, _Ftp._Password);
+                if (_FullPath != string.Empty)
+                {
+                    WebClient ftpClient = new WebClient();
+                    ftpClient.Credentials = new NetworkCredential(_Ftp._UserId, _Ftp._Password);
 
-                byte[] imageByte = ftpClient.DownloadData(_Path + "/" +_Name);
+                    byte[] imageByte = ftpClient.DownloadData(_Path + "/" + _Name);
 
-                MemoryStream mStream = new MemoryStream();
-                byte[] pData = imageByte;
-                mStream.Write(pData, 0, Convert.ToInt32(pData.Length));
-                Bitmap bm = new Bitmap(mStream, false);
-                mStream.Dispose();
+                    MemoryStream mStream = new MemoryStream();
+                    byte[] pData = imageByte;
+                    mStream.Write(pData, 0, Convert.ToInt32(pData.Length));
+                    Bitmap bm = new Bitmap(mStream, false);
+                    mStream.Dispose();
 
-                pictureBox1.Image = bm;
+                    pictureBox1.Image = bm;
+                }
+                else
+                {
+                    pictureBox1.Image = null;
+                }
             }
-            else
+            catch (Exception ex)
             {
-                pictureBox1.Image = null;
+                MessageBox.Show(ex.Message, ex.Source);
+                return;
+            }
+            finally
+            {
+
             }
         }
 
         private void MouseWheelEvent(object sender, MouseEventArgs e)
         {
-
-            var pictureBox = sender as PictureBox;
-
-            if (e.Delta > 0)
+            try
             {
-                pictureBox.Size = new Size((int)(pictureBox.Width * 1.2), (int)(pictureBox.Height * 1.2));
+                var pictureBox = sender as PictureBox;
+
+                if (e.Delta > 0)
+                {
+                    pictureBox.Size = new Size((int)(pictureBox.Width * 1.2), (int)(pictureBox.Height * 1.2));
+                }
+                else if (e.Delta < 0)
+                {
+                    pictureBox.Size = new Size((int)(pictureBox.Width * 0.8), (int)(pictureBox.Height * 0.8));
+                }
             }
-            else if (e.Delta < 0)
+            catch (Exception ex)
             {
-                pictureBox.Size = new Size((int)(pictureBox.Width * 0.8), (int)(pictureBox.Height * 0.8));
+                MessageBox.Show(ex.Message, ex.Source);
+                return;
+            }
+            finally
+            {
+
             }
         }
 
         private void pictureBox1_MouseUp(object sender, MouseEventArgs e)
         {
-            var c = sender as PictureBox;
-            if (null == c) return;
-            _Dragging = false;
+            try
+            {
+                var c = sender as PictureBox;
+                if (null == c) return;
+                _Dragging = false;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, ex.Source);
+                return;
+            }
+            finally
+            {
+
+            }
         }
 
         private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
         {
-            if (e.Button != MouseButtons.Left) return;
-            _Dragging = true;
-            _XPos = e.X;
-            _YPos = e.Y;
+            try
+            {
+                if (e.Button != MouseButtons.Left) return;
+                _Dragging = true;
+                _XPos = e.X;
+                _YPos = e.Y;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, ex.Source);
+                return;
+            }
+            finally
+            {
+
+            }
         }
 
         private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
         {
-            var c = sender as PictureBox;
-            if (!_Dragging || null == c) return;
-            c.Top = e.Y + c.Top - _YPos;
-            c.Left = e.X + c.Left - _XPos;
+            try
+            {
+                var c = sender as PictureBox;
+                if (!_Dragging || null == c) return;
+                c.Top = e.Y + c.Top - _YPos;
+                c.Left = e.X + c.Left - _XPos;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, ex.Source);
+                return;
+            }
+            finally
+            {
+
+            }
         }
     }
 }
